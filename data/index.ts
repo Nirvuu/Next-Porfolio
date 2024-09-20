e0289af81e5a1d3190b957363839c21f7a913033
Nirvuu <stwaynemani@gmail.com>
2024-09-21 01:22:07 +0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,205 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently developing Node JS Backend for an Iot Product",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Simple Blog Application",
+      des: "A React-based blog application that demonstrates basic CRUD functionality. Users can view, read, and delete blog posts, with data managed through a simple REST API using JSON Server.",
+      img: "/React Add New Blog-1.jpg",
+      iconLists: ["/html.svg","/css3-2.svg","/js.svg", "/re.svg"],
+      link: "https://github.com/Nirvuu/Simple-Blog-App",
+    },
+    {
+      id: 2,
+      title: " Real-time Weather Dashboard",
+      des: "A full-stack web application that displays current and hourly weather data based on user input, featuring secure JWT-based authentication and MySQL integration using Prisma ORM.",
+      img: "/Weather.jpg",
+      iconLists: ["/re.svg", "/nodejs.svg","/sql-final.svg","/Jwt.png",],
+      link: "https://github.com/Nirvuu/Weather-Application",
+    },
+
+  ];
+  
+  
+  export const testimonials = [
+    {
+      quote:
+        "Working with Usman on our MERN stack project was a great experience. His ability to take complex ideas and turn them into a smooth-running application was outstanding. His technical knowledge and collaborative approach make him an invaluable asset to any team.",
+      name: "James Williams",
+      title: "CTO of TechForward Innovations",
+    },
+    {
+      quote:
+        "Usman brought fresh insights to our Next.js project. His ability to streamline the development process and deliver ahead of schedule was impressive. Usman's strong work ethic and attention to detail were key to the success of our project.",
+      name: "William Head ",
+      title: "CEO of DigitalFlow Studios",
+    },
+    {
+      quote:
+        "Our eCommerce platform was built from scratch, and Usman made the entire process seamless. His deep understanding of Node.js and backend architecture ensured a fast and scalable solution for our business.",
+      name: "David Rodriguez",
+      title: "Founder of MarketWave Solutions",
+    },
+    {
+      quote:
+        "Usman’s expertise with React and API integration made a significant impact on our web application project. He quickly grasped our requirements and exceeded our expectations in delivering a high-quality, user-friendly product.",
+      name: "John Travolta",
+      title: "Product Manager at WebSphere Creatives",
+    },
+    {
+      quote:
+        "Usman's work on our mobile app was exceptional. His command of both frontend and backend technologies helped us deliver a powerful and intuitive solution for our users. I highly recommend working with him for any development needs.",
+      name: "Robert Miller",
+      title: "CEO of AppCraft Technologies",
+    },
+  ];
+  
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "MERN Stack Intern",
+      desc: "Developed backend APIs and designed an interactive UI for a travel log app using React.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "MERN Stack Developer",
+      desc: "Created API endpoints and managed database interactions for an IoT platform.",
+      className: "md:col-span-2",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Software Engineer",
+      desc: "Solved complex MIPS assembly problems for global clients.",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Freelance MERN Stack Developer",
+      desc: "Built professional web applications for international clients using the MERN stack.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6a1e01e5860e0c33018d1369eba92d65@o4507978947821568.ingest.us.sentry.io/4507978952474624",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
