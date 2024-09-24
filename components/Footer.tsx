61faee17e3a5fb36e411b160b7a3bab23d9aa144
Nirvuu <stwaynemani@gmail.com>
2024-09-24 14:12:23 +0500

    email fix again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="muhammadusman.ceme@gmail.com">
+        <a href="mailto:muhammadusman.ceme@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
